b374bdeb97b71d959e289a521a1b63bad8761cc8
Jesse Clark <contact@jessclark.com>
2015-07-14 09:18:41 -0600

    Add html and ts updates.

--- source/typescript/greeter.ts
+++ source/typescript/greeter.ts
@@ -1,7 +1,23 @@
-function greeter(person: string) {
-    return "Hello, " + person;
+class Student {
+    fullname : string;
+    constructor(public firstname : string, 
+    			public middleinitial  : string, 
+    			public lastname : string) {
+        this.fullname = firstname + " " + middleinitial + " " + lastname;
+    }
 }
 
-var user = "Jane User";
+interface Person {
+	firstname: string;
+	lastname: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstname + " " + person.lastname;
+    //return "Hello, " + person.fullname;
+}
+
+//var user = { firstname: "Jesse", lastname: "Clark" }
+var user = new Student("Jane", "M.", "User");
 
 document.body.innerHTML = greeter(user);
